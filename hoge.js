224cd8b7fcd007afb70535e9694745d1ded7ab99
shinmitt <shin0510100@gmail.com>
2023-09-27 23:44:40 +0900

    数当てゲームを作った

---
+++ hoge.js
@@ -0,0 +1,12 @@
+let correctNumber = Math.floor(Math.random() * 10) + 1;
+console.log(correctNumber); //デバッグ用。実際のゲームではコメントアウトした方が良いです。
+
+function checkNumber() {
+    let userInput = document.getElementById('userInput').value;
+    
+    if (userInput == correctNumber) {
+        document.getElementById('result').innerHTML = '正解です！';
+    } else {
+        document.getElementById('result').innerHTML = '違います。もう一度試してください！';
+    }
+}
